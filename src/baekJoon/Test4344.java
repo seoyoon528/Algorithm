2bc97beebbf0e0fe78315ddd3a262a6cf3556e85
seoyoon528 <seoyoon528@gmail.com>
2021-02-04 18:35:23 +0900

    [1차원 배열] 백준 4344:평균은 넘겠지

---
+++ src/baekJoon/Test4344.java
@@ -0,0 +1,41 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Test4344 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int C = in.nextInt();
+		double[] result = new double[C];
+		
+		for(int i = 0; i < C; i++) {
+			double avg = 0;
+			int sum = 0;
+			
+			int N = in.nextInt();
+			int[] score = new int[N];
+			
+			for(int j = 0; j < N; j++) {
+				score[j] = in.nextInt();
+				sum += score[j];
+			}
+			
+			avg = sum / N;
+
+			double up = 0; // up을 double로 선언하지 않으면 계산이 제대로 안나옴!
+			for(int k = 0; k < N; k++) {
+				if (score[k] > avg) {
+					up++;
+				}
+			}
+			
+			result[i] = (up / N) * 100;
+		}
+		
+		for (int i = 0; i < C; i++) {
+			System.out.printf("%.3f%%\n", result[i]); //'%'를 쓰기 위해선 %% 써야함
+		}
+	}
+
+}
