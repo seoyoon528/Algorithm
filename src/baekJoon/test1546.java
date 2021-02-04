fd6d63b027fc0edcb3c903ad49cb0aba997b4a7b
seoyoon528 <seoyoon528@gmail.com>
2021-02-04 16:10:28 +0900

    [1차원 배열] 백준 1546:평균

---
+++ src/baekJoon/test1546.java
@@ -0,0 +1,33 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class test1546 {
+
+	public static void main(String[] args) {
+		int N;
+		double avg = 0, max = 0, sum = 0;
+		Scanner in = new Scanner(System.in);
+		N = in.nextInt();
+		
+		double[] scores = new double[N];
+		
+		for(int i = 0; i < N; i++) {
+			scores[i] = in.nextDouble();
+
+			if (scores[i] > max) {
+				max = scores[i];
+			}
+		}
+		
+		for(int j = 0; j < N; j++) {
+			scores[j] = scores[j]/max*100;
+			sum += scores[j];
+		}
+		
+		avg = sum / N;
+		
+		System.out.println(avg);
+	}
+
+}
