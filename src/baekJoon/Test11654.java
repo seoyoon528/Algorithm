41582992b14a416f4817464f27a86fc68475385c
seoyoon528 <seoyoon528@gmail.com>
2021-07-17 12:35:36 +0900

    [문자열] 백준 11654:아스키 코드

---
+++ src/baekJoon/Test11654.java
@@ -0,0 +1,14 @@
+package baekJoon;
+import java.util.Scanner;
+
+public class Test11654 {
+
+	public static void main(String[] args) {		
+		Scanner scan = new Scanner(System.in);
+		
+        char x = scan.next().charAt(0);
+        System.out.println((int)x);
+
+	}
+
+}
