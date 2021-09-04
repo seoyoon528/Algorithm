e1ff5415416427289394a98aba6717f7f79cd34c
seoyoon528 <seoyoon528@gmail.com>
2021-09-05 00:13:23 +0900

    [백트래킹] 백준 15649:N과 M(1) -> 주석 추가 + 코드 수정

--- src/baekJoon/Test15649.java
+++ src/baekJoon/Test15649.java
@@ -18,7 +18,9 @@ public class Test15649 {
 		
 	}
 	
+	// DFS
 	public static void backtracking(int n, int m, int level) {
+		// level = 깊이
 		
 		if (level == m) {
 			for(int i = 0; i < arr.length; i++) {
@@ -26,14 +28,16 @@ public class Test15649 {
 			}
 			
 			System.out.println();
+			return;
 		}
 		
 		for(int i = 0; i < n; i++) {
-			if(check[i] == false) {
-				check[i] = true;
-				arr[level] = i + 1;
+			if(check[i] == false) { // 해당 노드에 방문하지 않았었다면?
+				check[i] = true; // 방문한 상태로 변경
+				arr[level] = i + 1; // 배열에 값 저장
+				
 				backtracking(n, m, level+1);
-				check[i] = false;
+				check[i] = false; // 자식 노드 방문이 모두 끝나면 다시 방문하지 않은 상태로 변경
 			}
 		}
 			
