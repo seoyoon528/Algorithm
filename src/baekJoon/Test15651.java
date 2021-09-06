edb3666aa8477c5553b6b1e124014074fee8f3ef
seoyoon528 <seoyoon528@gmail.com>
2021-09-06 17:28:55 +0900

    [백트래킹] 백준 15651:N과 M(3) (BufferedReader + StringBuilder)

---
+++ src/baekJoon/Test15651.java
@@ -0,0 +1,41 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test15651 {
+	
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+		backtracking(N, M, 0);
+		
+		System.out.print(sb);
+	}
+	
+	public static void backtracking(int n, int m, int level) {
+		if(level == m) {
+			for(int i = 0; i < arr.length; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+		} else {
+			for(int i = 1; i <= n; i++) {
+				arr[level] = i;
+				backtracking(n, m, level+1);
+			}
+		}
+	}
+
+}
