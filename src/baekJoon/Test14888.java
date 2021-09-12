d10ce5179268c1969794d66ab9f4b1a7c1111a22
seoyoon528 <seoyoon528@gmail.com>
2021-09-12 23:59:21 +0900

    [백트래킹] 백준 14888:연산자 끼워넣기 (input 단계)

--- src/baekJoon/Test14888.java
+++ src/baekJoon/Test14888.java
@@ -1,10 +1,18 @@
 package baekJoon;
 
+import java.util.Scanner;
+
 public class Test14888 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner  scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
 	}
 
 }
