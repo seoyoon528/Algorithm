8121b28f35e92ae860cec74a86131792b39a43d1
seoyoon528 <seoyoon528@gmail.com>
2021-09-13 00:01:42 +0900

    [백트래킹] 백준 14888:연산자 끼워넣기 (input 단계)

--- src/baekJoon/Test14888.java
+++ src/baekJoon/Test14888.java
@@ -7,10 +7,26 @@ public class Test14888 {
 	public static void main(String[] args) {
 		Scanner  scan = new Scanner(System.in);
 		int N = scan.nextInt();
+		int sum = 0;
+		
+		if(N < 2 || N > 11) {
+			System.out.println("It's out of range!");
+		}
+		
 		int[] arr = new int[N];
+		int[] operator = new int[4];
 		
 		for(int i = 0; i < N; i++) {
 			arr[i] = scan.nextInt();
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			operator[i] = scan.nextInt();
+			sum += operator[i];
+		}
+		
+		if(sum != N-1) {
+			System.out.println("Sum must be N-1");
 		}
 		
 	}
