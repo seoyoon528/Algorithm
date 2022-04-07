554ee21f3a61dc0a908ce99cffb3740d39d85c83
sypark <seoyoon528@gmail.com>
2022-04-07 23:54:43 +0900

    07.회문 문자열

---
+++ src/inflearn/String/src/IsPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class IsPalindrome {
+    public static void solution(String str) {
+        int i = 0;
+        boolean pal = true;
+        str = str.toLowerCase(Locale.ROOT);
+
+        while (i < str.length() / 2) {
+            if (str.charAt(i) != str.charAt(str.length()-(1+i))) {
+                pal = false;
+                break;
+            }
+            i++;
+        }
+        if (pal == true) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        solution(str);
+    }
+}
