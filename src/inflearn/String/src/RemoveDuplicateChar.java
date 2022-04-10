829fde9f8a32a3824557e7526328b172d7208ccc
sypark <seoyoon528@gmail.com>
2022-04-10 12:28:15 +0900

    06.중복 문자 제거

--- src/inflearn/String/src/RemoveDuplicateChar.java
+++ src/inflearn/String/src/RemoveDuplicateChar.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/* String-06
+  소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+  중복이 제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+*/
+
 public class RemoveDuplicateChar {
     public static String solution(String str) {
         String ans = "";
