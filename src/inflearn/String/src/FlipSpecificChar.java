3562d626ba9e1f1fd875434233d4554a065d7851
sypark <seoyoon528@gmail.com>
2022-04-10 12:25:34 +0900

    05.특정 문자 뒤집기

--- src/inflearn/String/src/FlipSpecificChar.java
+++ src/inflearn/String/src/FlipSpecificChar.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/* String-05
+  영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
+  특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+*/
+
 public class FlipSpecificChar {
     public static void solution(String str) {
         String[] ans = new String[str.length()];
