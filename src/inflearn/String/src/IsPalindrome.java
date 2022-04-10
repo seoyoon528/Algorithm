870c033853546f6707183a49131953fe75d8746a
sypark <seoyoon528@gmail.com>
2022-04-10 12:35:46 +0900

    07.회문 문자열

--- src/inflearn/String/src/IsPalindrome.java
+++ src/inflearn/String/src/IsPalindrome.java
@@ -1,6 +1,11 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+/* String-07
+  문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES",회문 문자열이 아니면 "NO"를 출력하는 프로그램을 작성하세요.
+  단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+*/
+
 public class IsPalindrome {
     public static void solution(String str) {
         int i = 0;
