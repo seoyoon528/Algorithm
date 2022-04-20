fe5993bbb68e155c0ca237a79acb85a39a3a107c
sypark <seoyoon528@gmail.com>
2022-04-21 00:09:30 +0900

    12.멘토링

--- src/inflearn/Array/src/Mentoring.java
+++ src/inflearn/Array/src/Mentoring.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
 
 /* Array-12
-   현수네 반 선생님은 반 학생들의 수학점수를 향상시키기 위해 멘토링 시스템을 만들려고 합니다.
-   멘토링은 멘토(도와주는 학생)와 멘티(도움을 받는 학생)가 한 짝이 되어 멘토가 멘티의 수학공부를 도와주는 것입니다.
-   선생님은 M번의 수학테스트 등수를 가지고 멘토와 멘티를 정합니다.
-   만약 A학생이 멘토이고, B학생이 멘티가 되는 짝이 되었다면, A학생은 M번의 수학테스트에서 모두 B학생보다 등수가 앞서야 합니다.
+ * 현수네 반 선생님은 반 학생들의 수학점수를 향상시키기 위해 멘토링 시스템을 만들려고 합니다.
+ * 멘토링은 멘토(도와주는 학생)와 멘티(도움을 받는 학생)가 한 짝이 되어 멘토가 멘티의 수학공부를 도와주는 것입니다.
+ * 선생님은 M번의 수학테스트 등수를 가지고 멘토와 멘티를 정합니다.
+ * 만약 A학생이 멘토이고, B학생이 멘티가 되는 짝이 되었다면, A학생은 M번의 수학테스트에서 모두 B학생보다 등수가 앞서야 합니다.
+ * M번의 수학성적이 주어지면 멘토와 멘티가 되는 짝을 만들 수 있는 경우가 총 몇 가지 인지 출력하는 프로그램을 작성하세요.
  */
 
 public class Mentoring {
     public static void solution(int n, int m, int[][] arr) {
         int ans = 0;
-
-        for (int i = 1; i <= m; i++) {      // i번 학생
-            for (int j = 1; j <= n; j++) {      // j번 학생
+        for (int i = 1; i <= n; i++) {      // i번 학생 (멘토)
+            for (int j = 1; j <= n; j++) {      // j번 학생 (멘티)
                 int cnt = 0;
                 for (int k = 0; k < m; k++) {
-                    int pi=0, pj=0;     // pi :: i의 position, pj :: j의 position
+                    int pi = 0, pj = 0;     // pi :: i의 position, pj :: j의 position
 
                     for (int s = 0; s < n; s++) {
-                        if (arr[k][s] == i) {
-                            pi = s;
-                        }
-                        if (arr[k][s] == j) {
-                            pj = s;
-                        }
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
                     }
                     if (pi < pj) {
                         cnt++;
