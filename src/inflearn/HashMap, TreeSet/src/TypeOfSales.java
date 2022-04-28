0701d06abc1968007ab301fa96fcdaa8340fd76f
sypark <seoyoon528@gmail.com>
2022-04-29 00:17:26 +0900

    03.매출액의 종류

--- src/inflearn/HashMap, TreeSet/src/TypeOfSales.java
+++ src/inflearn/HashMap, TreeSet/src/TypeOfSales.java
@@ -2,6 +2,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/* HashMap, TreeSet-03
+ * 현수의 아빠는 제과점을 운영합니다.
+ * 현수아빠는 현수에게 N일 동안의 매출기록을 주고 연속된 K일 동안의 매출액의 종류를 각 구간별로 구하라고 했습니다.
+ * 만약 N=7이고 7일 간의 매출기록이 아래와 같고, 이때 K=4이면
+ * 20 12 20 10 23 17 10
+ * 각 연속 4일간의 구간의 매출종류는
+ * 첫 번째 구간은 [20, 12, 20, 10]는 매출액의 종류가 20, 12, 10으로 3이다.
+ * 두 번째 구간은 [12, 20, 10, 23]는 매출액의 종류가 4이다.
+ * 세 번째 구간은 [20, 10, 23, 17]는 매출액의 종류가 4이다.
+ * 네 번째 구간은 [10, 23, 17, 10]는 매출액의 종류가 3이다.
+ * N일간의 매출기록과 연속구간의 길이 K가 주어지면 첫 번째 구간부터 각 구간별
+ * 매출액의 종류를 출력하는 프로그램을 작성하세요.
+ */
+
 public class TypeOfSales {
     public static void solution(int n, int k, int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -18,8 +32,8 @@ public class TypeOfSales {
 
             if (map.get(arr[lt]) == 0) {
                 map.remove(arr[lt]);
-                lt++;
             }
+            lt++;
         }
         System.out.println(ans);
     }
