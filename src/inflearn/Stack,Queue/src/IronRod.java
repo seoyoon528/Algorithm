3d6b881f87ee95c6b8e5bba12413333fe26894a7
sypark <seoyoon528@gmail.com>
2022-05-05 23:14:57 +0900

    05.쇠막대기

---
+++ src/inflearn/Stack,Queue/src/IronRod.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class IronRod {
+    public static void solution(String str) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        solution(str);
+    }
+}
