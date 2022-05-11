c6091156603aa562810850a52ca2bcf53d11e43d
sypark <seoyoon528@gmail.com>
2022-05-12 00:37:22 +0900

    02.버블정렬

---
+++ src/inflearn/Sorting,Searching/src/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/* Sorting,Searching-02
+ * N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 버블정렬입니다.
+ */
+
+public class BubbleSort {
+    public static int[] solution(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort T = new SelectionSort();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(arr)) System.out.print(x + " ");
+    }
+}
