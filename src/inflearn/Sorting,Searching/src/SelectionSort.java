1bc4d5511fad5b216418b62a73ec76d74a3d8d5b
sypark <seoyoon528@gmail.com>
2022-05-11 23:54:47 +0900

    01.선택정렬

---
+++ src/inflearn/Sorting,Searching/src/SelectionSort.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/* Sorting,Searching-01
+ * N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 선택정렬입니다.
+ */
+
+public class SelectionSort {
+    public static int[] solution(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            if (i != min) {
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort T = new SelectionSort();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(arr)) System.out.print(x + " ");
+    }
+}
+
