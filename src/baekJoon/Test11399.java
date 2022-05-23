2d05935defc85b31f8690cf32fa1b0190c62a83b
sypark <seoyoon528@gmail.com>
2022-05-24 00:42:47 +0900

    [정렬] 백준 11399:ATM

---
+++ src/baekJoon/Test11399.java
@@ -0,0 +1,34 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Test11399 {
+    public static int solution(int n, int[] arr) {
+        int ans = 0;
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j ++) {
+                ans += arr[j];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+}
