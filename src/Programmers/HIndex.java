0d12599738ddc17482dd34873623574c53d8a184
sypark <seoyoon528@gmail.com>
2022-05-26 03:29:20 +0900

    [정렬] 프로그래머스:H-Index

--- src/Programmers/HIndex.java
+++ src/Programmers/HIndex.java
@@ -2,22 +2,42 @@ import java.util.Arrays;
 
 public class HIndex {
     public static int solution(int[] citations) {
-        int answer = 0, h;
+        // {6, 5, 3, 1, 0} -> {0, 1, 3, 5, 6}
+        // 0 ->
+        // 1 -> 6
+        // 2 -> 5
+        // 3 -> 3 H-Index
+        // 4 -> 1
+
+        // {10, 8, 5, 4, 3} -> {3, 4, 5, 8, 10}
+        // 0 ->
+        // 1 -> 10
+        // 2 -> 8
+        // 3 -> 5
+        // 4 -> 4 H-Index
+        // 5 -> 3
+
+        // {12, 11, 10, 9, 8, 1} -> {1, 8, 9, 10, 11, 12}
+        // 0 ->
+        // 1 -> 12
+        // 2 -> 11
+        // 3 -> 10
+        // 4 -> 9
+        // 5 -> 8 H-Index
+        // 6 -> 1
+
+        int answer = 0;
         Arrays.sort(citations);
 
         for (int i = 0; i < citations.length; i++) {
-            h = citations.length - i;
-
-            if (citations[i] >= h) {
-                answer = h;
-                break;
-            }
+            int min = Math.min(citations[i], citations.length-i);
+            answer = Math.max(min, answer);
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] {3, 0, 6, 1, 5};
+        int[] arr = new int[] {4, 4, 4};
         System.out.println(solution(arr));
     }
 }
